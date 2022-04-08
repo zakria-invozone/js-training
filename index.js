ef0ea3da677775cd0b99bcff8d01eb3bcedd7352
zakria-invozone <103173679+zakria-invozone@users.noreply.github.com>
2022-04-08 11:59:44 +0500

    JS Operators

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-https://docs.google.com/document/d/1zWx9T131e1a5FZMAEWKezoEvgyo_4hfdQd8aoGo4_C4/edit#
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+
+// let x = 5;
+// let y = 2;
+// let z = x / y;
+
+// let x = 5;
+// let y = 2;
+// let z = x % y;
+
+// let x = 5;
+// x++;
+// let z = x;
+
+// let x = 5;
+// x--;
+// let z = x;
+
+// let x = 5;
+// x--;
+// let z = x;
+
+let x = 5;
+let z = Math.pow(x,2);   // result is 25
+
+
+/*Comparison operators
+   
+*/
+
+console.log(z);
+
+//Multiplication (*) and division (/) have higher precedence than addition (+) and subtraction (-).
+
